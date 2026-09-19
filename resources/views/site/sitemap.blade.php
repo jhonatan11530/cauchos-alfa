6d94a63ef05b24fcc6679752e6b7dc5524e385a4
jhonatan fernandez <jhonatan.fernandez1153@gmail.com>
2026-09-19 14:57:04 -0500

    mejora sitemap

--- resources/views/site/sitemap.blade.php
+++ resources/views/site/sitemap.blade.php
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ route('site.home') }}</loc>
@@ -21,4 +22,3 @@
     </url>
     @endforeach
 </urlset>
-

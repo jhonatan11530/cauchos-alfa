ccaad3d3c771805806816db65a47aa68a9bb924b
jhonatan fernandez <jhonatan.fernandez1153@gmail.com>
2026-09-22 21:25:41 -0500

    mejora en imagenes

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -172,7 +172,6 @@
                                         </div>
                                     @elseif (count($urls) === 1)
                                         <img src="{{ $urls[0] }}" class="card-img-top w-100"
-                                            alt="{{ $product->name }}" style="aspect-ratio: 4 / 3; object-fit: cover;">
                                             alt="{{ $product->name }}" loading="lazy" style="aspect-ratio: 4 / 3; object-fit: cover;">
                                     @else
                                         <svg class="card-img-top w-100"
